6695413c09bccabe469bdd9afcb1f8be021c0d2e
Kalyan5252 <kalyanpendem007@gmail.com>
2025-03-05 19:11:19 +0530

    model swtich

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -8,7 +8,7 @@ const Hero = () => {
   return (
     <div className="relative top-0 w-screen grid grid-cols-2 h-screen bg-gradient-to-bl from-[#192e35] to-[#04080a]">
       <Intro />
-      {/* <HeroScene /> */}
+      <HeroScene />
       <MouseScroll />
       <div className="absolute bottom-15 h-5 w-full bg-try blur-3xl opacity-70"></div>
       <div className="absolute bottom-0 h-10 w-full herodispersion blur-3xl"></div>
